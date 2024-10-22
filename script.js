6f68b075bc0eab7adce71af4721df35a31ee8f3d
Artem Cherkashin <157517719+Efenzik@users.noreply.github.com>
2024-10-23 00:30:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function showImage() {
+  // Скрываем кнопку
+  document.getElementById("showImageBtn").style.display = "none";
+
+  // Показываем картинку
+  document.getElementById("hiddenImage").style.display = "block";
+}

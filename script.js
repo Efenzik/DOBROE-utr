a58af18410dd61ee94f9b9be3b4c62d481f8c434
Artem Cherkashin <157517719+Efenzik@users.noreply.github.com>
2025-07-22 22:07:54 +0300

    .

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 function showImage() {
-  // Скрываем кнопку
   document.getElementById("showImageBtn").style.display = "none";
-
-  // Показываем картинку
   document.getElementById("hiddenImage").style.display = "block";
+
+  const audio = document.getElementById("myAudio");
+  audio.volume = 0.05;
+  audio.play();
+
+  startEmojiRain();
+}
+
+function startEmojiRain() {
+  const container = document.getElementById("emojiRain");
+  const emojis = ["🐳", "💋", "👅", "🛐", "🔞", "😳", "🥵", "😈"];
+  setInterval(() => {
+    const numberOfEmojis = Math.floor(Math.random() * 10) + 20;
+
+    for (let i = 0; i < numberOfEmojis; i++) {
+      const emoji = document.createElement("div");
+      emoji.classList.add("emoji");
+      emoji.textContent = emojis[Math.floor(Math.random() * emojis.length)];
+
+      emoji.style.left = Math.random() * 100 + "vw";
+      emoji.style.fontSize = Math.random() * 20 + 20 + "px";
+      emoji.style.animationDuration = Math.random() * 2 + 2 + "s";
+
+      container.appendChild(emoji);
+
+      setTimeout(() => {
+        emoji.remove();
+      }, 5000);
+    }
+  }, 500);
 }
